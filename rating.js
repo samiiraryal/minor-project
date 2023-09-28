6037729efd0793adc8b42f36262cea2b7d792b92
samiiraryal <95860466+samiiraryal@users.noreply.github.com>
2023-09-28 18:57:45 +0545

    new rating section added

--- rating.js
+++ rating.js
@@ -1,102 +1,101 @@
-// Sample player data (you can replace this with your own data)
-const playersData = [
-    { name: "Player 1", rating: 85, position: "Forward" },
-    { name: "Player 2", rating: 90, position: "Midfielder" },
-    { name: "Player 3", rating: 82, position: "Defender" },
-    { name: "Player 4", rating: 88, position: "Midfielder" },
-    { name: "Player 5", rating: 87, position: "Forward" },
-    { name: "Player 6", rating: 84, position: "Defender" },
-    { name: "Player 7", rating: 89, position: "Midfielder" },
-    { name: "Player 8", rating: 83, position: "Forward" },
-    { name: "Player 9", rating: 86, position: "Defender" },
-    { name: "Player 10", rating: 91, position: "Midfielder" },
-    { name: "Player 11", rating: 90, position: "Goalkeeper" },
-    { name: "Player 12", rating: 84, position: "Midfielder" },
-    { name: "Player 13", rating: 82, position: "Defender" },
-    { name: "Player 14", rating: 88, position: "Forward" },
-    { name: "Player 15", rating: 87, position: "Defender" },
-    { name: "Player 16", rating: 89, position: "Midfielder" },
-    { name: "Player 17", rating: 83, position: "Forward" },
-    // Add more player data as needed
-];
+// Sample player data for Team A and Team B (you can replace this with your own data)
+const teamAPlayers = [];
+const teamBPlayers = [];
 
-// Initialize lineup and substitutes arrays
-let lineup = playersData.slice(0, 11); // First 11 players as lineup
-let substitutes = playersData.slice(11, 17); // Next 6 players as substitutes
-
-// Variables to keep track of the selected playing player and selected substitute
-let selectedPlayingPlayer = null;
-let selectedSubstitutePlayer = null;
-
-// Variables to keep track of the substituted-in and swapped-out player indices
-let substituteInPlayerIndex = null;
-let substitutedOutPlayerIndex = null;
-
-// Function to render the lineup and substitutes lists
-function renderLineupAndSubstitutes() {
-    const lineupList = document.getElementById("lineup-list");
-    lineupList.innerHTML = `
-        <li class="player-header">
-            <span class="player-name">Name</span>
-            <span class="player-position">Position</span>
-            <span class="player-rating">Rating</span>
-            <span class="player-actions">Actions</span>
-        </li>
-    `;
-
-    lineup.forEach((player, index) => {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `
-            <span class="player-name ${selectedPlayingPlayer === index ? 'highlighted-playing' : ''}">${player.name}</span>
-            <span class="player-position">${player.position}</span>
-            <span class="player-rating">${player.rating}</span>
-            <button onclick="handleSubstitute(${index})">Substitute</button>
-        `;
-        lineupList.appendChild(listItem);
-    });
-
-    const substitutesList = document.getElementById("substitutes-list");
-    substitutesList.innerHTML = `
-        <li class="player-header">
-            <span class="player-name">Name</span>
-            <span class="player-position">Position</span>
-            <span class="player-rating">Rating</span>
-            <span class="player-actions">Actions</span>
-        </li>
-    `;
-
-    substitutes.forEach((player, index) => {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `
-            <span class="player-name ${selectedSubstitutePlayer === index ? 'highlighted-substitute' : ''}">${player.name}</span>
-            <span class="player-position">${player.position}</span>
-            <span class="player-rating">${player.rating}</span>
-            <button onclick="handleSubstituteSelect(${index})">Select</button>
-        `;
-        substitutesList.appendChild(listItem);
-    });
+// Generate 24 players for Team A
+for (let i = 1; i <= 24; i++) {
+    teamAPlayers.push({ name: `A${i}` });
 }
 
-// Function to handle the substitute action when the "Substitute" button is clicked
-function handleSubstitute(playerIndex) {
-    selectedPlayingPlayer = playerIndex;
-    selectedSubstitutePlayer = null; // Reset the selected substitute
-    renderLineupAndSubstitutes(); // Update the display
+// Generate 24 players for Team B
+for (let i = 1; i <= 24; i++) {
+    teamBPlayers.push({ name: `B${i}` });
 }
 
-// Function to handle the selection of a substitute
-function handleSubstituteSelect(substituteIndex) {
-    if (selectedPlayingPlayer !== null) {
-        const temp = lineup[selectedPlayingPlayer];
-        lineup[selectedPlayingPlayer] = substitutes[substituteIndex];
-        substitutes[substituteIndex] = temp;
-        substituteInPlayerIndex = selectedPlayingPlayer; // Highlight the substituted-in player in green
-        substitutedOutPlayerIndex = substituteIndex; // Highlight the swapped-out player in red
-        selectedPlayingPlayer = null;
-        selectedSubstitutePlayer = null; // Reset the selected substitute
-        renderLineupAndSubstitutes();
+// Get the player list containers for Team A and Team B
+const teamAList = document.getElementById('team-a-list');
+const teamBList = document.getElementById('team-b-list');
+
+// Get the selected players list containers
+const selectedPlayersListA = document.getElementById('selected-players-a');
+const selectedPlayersListB = document.getElementById('selected-players-b');
+
+// Function to create a player item with checkbox
+function createPlayerItem(player, team) {
+    const playerItem = document.createElement('div');
+    playerItem.className = 'player-item';
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'player-checkbox';
+
+    checkbox.addEventListener('click', () => {
+        handleCheckboxClick(player, team);
+    });
+
+    const playerName = document.createElement('span');
+    playerName.textContent = player.name;
+
+    playerItem.appendChild(checkbox);
+    playerItem.appendChild(playerName);
+
+    return playerItem;
+}
+
+// Function to handle checkbox click event
+function handleCheckboxClick(player, team) {
+    const selectedPlayerItem = document.createElement('li');
+    selectedPlayerItem.textContent = player.name;
+
+    if (team === 'A') {
+        if (selectedPlayersListA.children.length < 11) {
+            selectedPlayersListA.appendChild(selectedPlayerItem);
+        } else {
+            alert('Team A can only select up to 11 players.');
+            player.checkbox.checked = false;
+        }
+    } else if (team === 'B') {
+        if (selectedPlayersListB.children.length < 11) {
+            selectedPlayersListB.appendChild(selectedPlayerItem);
+        } else {
+            alert('Team B can only select up to 11 players.');
+            player.checkbox.checked = false;
+        }
     }
 }
 
-// Initial rendering of lineup and substitutes
-renderLineupAndSubstitutes();
+// Function to handle Select Players button click event
+function handleSelectPlayersClick() {
+    const selectedCountA = selectedPlayersListA.children.length;
+    const selectedCountB = selectedPlayersListB.children.length;
+
+    if (selectedCountA === 11 && selectedCountB === 11) {
+        alert('Selected players for Team A: ' + selectedCountA + '\nSelected players for Team B: ' + selectedCountB);
+    } else {
+        alert('Please select 11 players for each team.');
+    }
+}
+
+// Function to render the player list for Team A
+function renderTeamAPlayerList() {
+    teamAPlayers.forEach(player => {
+        const playerItem = createPlayerItem(player, 'A');
+        teamAList.appendChild(playerItem);
+    });
+}
+
+// Function to render the player list for Team B
+function renderTeamBPlayerList() {
+    teamBPlayers.forEach(player => {
+        const playerItem = createPlayerItem(player, 'B');
+        teamBList.appendChild(playerItem);
+    });
+}
+
+// Call the render functions to display the initial player lists for both teams
+renderTeamAPlayerList();
+renderTeamBPlayerList();
+
+// Add event listener to the Select Players button
+const selectPlayersButton = document.getElementById('select-players-btn');
+selectPlayersButton.addEventListener('click', handleSelectPlayersClick);
